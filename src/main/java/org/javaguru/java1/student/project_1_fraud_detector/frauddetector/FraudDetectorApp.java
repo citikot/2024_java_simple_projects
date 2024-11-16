b8a5c772695ee34643b2c2d32c23d117f68bc33d
Alex Pishvanov <citikot@dr.com>
2024-11-16 13:54:31 +0200

    changed main app name

--- src/main/java/org/javaguru/java1/student/project_1_fraud_detector/frauddetector/FraudDetectorApp.java
+++ src/main/java/org/javaguru/java1/student/project_1_fraud_detector/frauddetector/FraudDetectorApp.java
@@ -1,6 +1,6 @@
 package org.javaguru.java1.student.project_1_fraud_detector.frauddetector;
 
-public class DetectorApp {
+public class FraudDetectorApp {
     public static void main(String[] args) {
         System.out.println("Detector started!");
     }
