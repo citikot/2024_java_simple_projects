ec61bb5d8930208b95194aaaad31c940a1aa098d
Olelsandr Pishvanov <oleksandrp@paynexpay.com>
2024-11-18 23:38:37 +0200

    added init test data method

--- src/test/java/org/javaguru/java1/student/project_1_fraud_detector/frauddetector/domain/FraudDetectorTest.java
+++ src/test/java/org/javaguru/java1/student/project_1_fraud_detector/frauddetector/domain/FraudDetectorTest.java
@@ -1,12 +1,38 @@
 package org.javaguru.java1.student.project_1_fraud_detector.frauddetector.domain;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FraudDetectorTest {
 
     private static final FraudDetector fraudDetector = new FraudDetector();
+    private static final List<Transaction> rawTransactions = new ArrayList<>();
+    private static List<Trader> traders = new ArrayList<>();
+
+    @BeforeAll
+    static void init() {
+        traders = List.of(
+                new Trader("John Dow", "New York"),
+                new Trader("Mari Ell", "Minnesota"),
+                new Trader("P Did", "Los Angeles"),
+                new Trader("Jess Alba", "Hollywood"),
+                new Trader("Emily Radkovsky", "Pasadena"),
+                new Trader("Dominic Torrent", "Seattle"),
+                new Trader("Pokemon", "Tokyo")
+        );
+
+        List<Integer> transactionAmounts = List.of( 1000, 5000, 1000000, 2000000, 50000, 999999, 1);
+
+        for (int i = 0; i < traders.size(); i++) {
+            rawTransactions.add(new Transaction(traders.get(i), transactionAmounts.get(i)));
+        }
+    }
 
     @Test
     void testTransactionTradersIsFraudster() {
@@ -33,5 +59,4 @@ class FraudDetectorTest {
         // then
         assertFalse(isFraudster);
     }
-
 }
